51f8d8813cc9c54c64a587fb10ab42e5b5fb94da
NigelAntonPieter <na.pieter@hotmail.nl>
2022-06-15 11:29:03 +0200

    sprint 2
    kassabon gemaakt met een observationCollection en een nieuwe list

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
     <div class="container header">
         <img src="<?php echo $base_url; ?>/img/logo-big-v4.png" alt="logo" class="logo">
-        <h1>Attracties</h1>
+        <h1>Developerland/Attracties</h1>
         <nav>
             <a href="<?php echo $base_url; ?>/index.php">Attracties</a> |
             <a href="<?php echo $base_url; ?>/admin/index.php">Admin</a>
--- index.php
+++ index.php
@@ -4,30 +4,88 @@ require_once 'admin/backend/config.php';
 ?>
 
 <!doctype html>
-<html lang="nl">
+    <html lang="nl">
 
-<head>
-    <title>Attractiepagina</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
-    <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
-    <link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon" />
-</head>
+    <head>
+        <title>Attractiepagina</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
+        <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
+        <link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon" />
+    </head>
 
-<body>
+    <body>
 
-    <?php require_once 'header.php'; ?>
-    <div class="container content">
-        <aside>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
-        </aside>
-        <main>
-            <!-- hier komen de attractiekaartjes -->
+        <?php require_once 'header.php'; ?>
+        <div class="container content">
+            <aside>
+
+
+            </aside>
+            <main>
+               <?php require_once 'admin/backend/conn.php';
+
+               $query = "SELECT * FROM rides";
+               $statement = $conn->prepare($query);
+               $statement->execute();
+               $attracties = $statement ->fetchAll(PDO::FETCH_ASSOC);
+
+               ?>
+
+               <div class="container_grid">
+                <?php foreach($attracties as $attractie): ?>
+                    <div class="rideCard">
+                        <img src="img/attracties/<?php echo $attractie['img_file']; ?> " alt="">
+                        <div class="theme"> 
+                            <p> <?php echo $attractie['themeland'];?></p>
+
+                        </div>  
+                            <div class="title"> 
+                                <p> <?php echo $attractie['title'];?></p>
+                            </div>
+                            <div class="des">   
+                                <p> <?php echo $attractie['description'];?></p>
+                            </div>
+                            
+                            
+                        
+                    </div>
+
+
+                    <div class="rideCard Large">
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard">      
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard">
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard">
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard Large">
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard Large">
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard">
+                        <p> t</p>
+                    </div>
+                    <div class="rideCard">
+                        <p> t</p>
+                    </div>
+                    
+                <?php endforeach; ?>
+            </div>
+
         </main>
     </div>
+
 
 </body>
 
